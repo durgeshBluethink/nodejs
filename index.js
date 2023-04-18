f8134078a17d2b359f92e63f8f688dd8b6782e3f
Siva <rsivakoteswararao@gmail.com>
2023-04-18 19:01:29 +0530

    this is file create in dynmic form

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // console.log(process.argv[1])
 const fs = require("fs");
- const input =  process.argv;
-//  fs.writeFileSync(input[2],input[3]);
-if(input[2]=='add'){
-    fs.writeFileSync(input[3],input[4])
-}else if(input[2]=='remove'){
-    fs.unlinkSync(input[3])
-}else{
-    console.log("invalid input")
-}
 
+ const path = require('path');
+ const dirPath  = path.join(__dirname,'files');
+ for (let index = 0; index < 5; index++) {
+    // create a dynmic file in spafic folder
+    // fs.writeFileSync(dirPath+"/hello"+index+".txt","this is simple file in dynmic form we are create !");
+    fs.writeFileSync(dirPath+`/hello ${index}.txt`,"this is simple file in dynmic form we are create !");
+    
+ }
 
 
 
