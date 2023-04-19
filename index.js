a453b33f380192f6a116b1dcd78eca1e5bcd2c92
durgeshBluethink <durgesh1.bluethink@gmail.com>
2023-04-19 12:04:43 +0530

    nothing

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const fs = require("fs");
 
  const path = require('path');
  const dirPath  = path.join(__dirname,'files');
- for (let index = 0; index < 5; index++) {
-    // create a dynmic file in spafic folder
-    // fs.writeFileSync(dirPath+"/hello"+index+".txt","this is simple file in dynmic form we are create !");
-    fs.writeFileSync(dirPath+`/hello ${index}.txt`,"this is simple file in dynmic form we are create !");
+//  for (let index = 0; index < 5; index++) {
+//     // create a dynmic file in spafic folder
+//     // fs.writeFileSync(dirPath+"/hello"+index+".txt","this is simple file in dynmic form we are create !");
+//     fs.writeFileSync(dirPath+`/hello ${index}.txt`,"this is simple file in dynmic form we are create !");
     
- }
+//  }
+
+fs.readdir(dirPath,(err,files)=>{
+ //   console.warn(files) // file get in array list
+ files.forEach((item)=>{
+    console.log
+ })
+})
 
 
 
